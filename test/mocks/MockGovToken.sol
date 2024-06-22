0964b22f14367f63165040b60da8ab3d68371186
Ana Julia <anajuliabit@gmail.com>
2024-06-22 15:50:16 -0300

    rename

--- test/mocks/MockGovToken.sol
+++ test/mocks/MockGovToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.25;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockShu is ERC20 {
+contract MockGovToken is ERC20 {
     constructor() ERC20("Shu", "SHU") {
         _mint(msg.sender, 5_000_000 * 1e18);
     }
