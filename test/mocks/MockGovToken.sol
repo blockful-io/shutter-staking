842dcd49087de84b95a9539f3ff508f2530c1299
Ana Julia <anajuliabit@gmail.com>
2024-06-22 16:57:14 -0300

    update govtoken

--- test/mocks/MockGovToken.sol
+++ test/mocks/MockGovToken.sol
@@ -5,9 +5,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockGovToken is ERC20 {
-    constructor() ERC20("Shu", "SHU") {
-        _mint(msg.sender, 5_000_000 * 1e18);
-    }
+    constructor() ERC20("Shu", "SHU") {}
 
     function mint(address account, uint256 amount) external {
         _mint(account, amount);
