b0522283d522de031e9c18e66814f06f9f9f6ac1
Ana Julia <anajuliabit@gmail.com>
2024-06-30 17:36:11 -0300

    lint

--- test/helper/ProxyUtils.sol
+++ test/helper/ProxyUtils.sol
@@ -5,9 +5,9 @@ import {Vm} from "@forge-std/Vm.sol";
 import {ERC1967Utils} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
 
 library ProxyUtils {
-    address constant CHEATCODE_ADDRESS =
+    address public constant CHEATCODE_ADDRESS =
         0x7109709ECfa91a80626fF3989D68f67F5b1DD12D;
-    Vm constant vm = Vm(CHEATCODE_ADDRESS);
+    Vm public vm = Vm(CHEATCODE_ADDRESS);
 
     function getAdminAddress(address proxy) public view returns (address) {
         bytes32 adminSlot = vm.load(proxy, ERC1967Utils.ADMIN_SLOT);
